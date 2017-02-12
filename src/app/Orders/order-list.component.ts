c5303d893e607ceb2c76ea7d32fadc4a148a48c2
AK <kasssalex@gmail.com>
2017-02-12 12:56:30 +0100

    orders: search by sapId

--- src/app/Orders/order-list.component.ts
+++ src/app/Orders/order-list.component.ts
@@ -68,7 +68,7 @@ export class OrderListComponent implements OnInit {
                     || order.annotation.supplier.toUpperCase().includes(txt)
                     || (order.annotation.equipe && order.annotation.equipe.toUpperCase().includes(txt))
                     || order.annotation.status.toUpperCase().includes(txt)
-                    || order.data.kid === +txt;
+                    || order.data.kid === +txt || order.data.sapId === +txt;
 
             }).slice(0,200).map(order => {
                 let x= order;
