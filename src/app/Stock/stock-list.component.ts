d8a9a9265000899929fac9ce46cd2b31f1f7a3e7
AK <kasssalex@gmail.com>
2017-02-21 23:35:22 +0100

    search box in stock

--- src/app/Stock/stock-list.component.ts
+++ src/app/Stock/stock-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
 import { ProductService } from './../Shared/Services/product.service'
 import { Observable, Subscription } from 'rxjs/Rx'
 import { NgbPanelChangeEvent } from '@ng-bootstrap/ng-bootstrap';
@@ -15,7 +16,9 @@ import { NgbPanelChangeEvent } from '@ng-bootstrap/ng-bootstrap';
 )
 export class StockListComponent implements OnInit {
     constructor() {
-
+        this.searchForm = new FormGroup({
+            searchControl: new FormControl()
+        });
     }
 
     private products; //: Observable<any>;
@@ -23,53 +26,59 @@ export class StockListComponent implements OnInit {
     @Input() state;
     @Input() path: string
     @Input() accentOnOrdering: boolean = true
-    @Output() stateChanged= new EventEmitter();
+    @Output() stateChanged = new EventEmitter();
 
     subscriptionProducts: Subscription
+    searchControl = new FormControl();
+    searchForm;
 
-
-    private stateInit()
-    {
-        if (!this.state) this.state= {};
+    private stateInit() {
+        if (!this.state) this.state = {};
         if (!this.state.openPanelId) this.state.openPanelId = '';
     }
 
     ngOnInit(): void {
         this.stateInit();
-        this.subscriptionProducts= this.productsObservable.subscribe(products => 
-            this.products = products);
+
+        this.subscriptionProducts = Observable.combineLatest(this.productsObservable, this.searchControl.valueChanges.debounceTime(400).distinctUntilChanged().startWith(''), (products, searchTxt: string) => {
+            if (searchTxt.trim() === '') return products;
+            return products.filter(product => product.values[0].annotation.product && (product.values[0].annotation.product.toUpperCase().includes(searchTxt.toUpperCase()) || 
+                                            product.values[0].annotation.supplier.toUpperCase().includes(searchTxt.toUpperCase())));
+        }).subscribe(products => this.products = products);
     }
 
     ngOnDestroy(): void {
-         this.subscriptionProducts.unsubscribe()
+        this.subscriptionProducts.unsubscribe()
     }
 
     getProductObservable(id: string): Observable<any> {
-        return this.productsObservable.map(products => 
-        {
-            let product= products.filter(s => s.key === id)[0];
-            return product; 
-        } );
+        return this.productsObservable.map(products => {
+            let product = products.filter(s => s.key === id)[0];
+            return product;
+        });
     }
 
-    nbAvailable(product){
-        return product.values.reduce((acc, b)=> acc + b.annotation.nbAvailable, 0);
+    nbAvailable(product) {
+        return product.values.reduce((acc, b) => acc + b.annotation.nbAvailable, 0);
+    }
+
+    resetSerachControl() {
+        this.searchControl.setValue('')
     }
 
 
-   // This is typically used for accordions with ngFor, for remembering the open Accordion Panel (see template as well)
+    // This is typically used for accordions with ngFor, for remembering the open Accordion Panel (see template as well)
     private beforeAccordionChange($event: NgbPanelChangeEvent) {
-        if ($event.nextState)
-        {
+        if ($event.nextState) {
             this.state.openPanelId = $event.panelId;
             this.stateChanged.next(this.state);
-        }            
+        }
     };
 
     // This is typically used for accordions with ngFor and tabsets in the cild component. As the ngFor disposes and recreates the child component, we need a way to remember the opened tab
-    private childStateChanged(newState, objectId)
-    {
-            this.state[objectId]= newState;
-            this.stateChanged.next(this.state);
-    }}
+    private childStateChanged(newState, objectId) {
+        this.state[objectId] = newState;
+        this.stateChanged.next(this.state);
+    }
+}
 
