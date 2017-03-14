dbb90342fb40d3d8f2d28cd711347db2b441c698
kresiak <kresiak1@gmail.com>
2017-03-14 21:32:03 +0100

    supplier detail component

--- src/app/Suppliers/supplier-info.component.ts
+++ src/app/Suppliers/supplier-info.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core'
-import { ActivatedRoute, Params, Router } from '@angular/router'
-import { ProductService } from './../Shared/Services/product.service'
-import { OrderService } from './../Shared/Services/order.service'
-import { SupplierService } from './../Shared/Services/supplier.service'
-import { AuthenticationStatusInfo, AuthService } from './../Shared/Services/auth.service'
-import { Observable, Subscription } from 'rxjs/Rx'
 
 @Component(
     {
@@ -14,13 +8,9 @@ import { Observable, Subscription } from 'rxjs/Rx'
 )
 
 export class SupplierInfoComponent implements OnInit {
-    ngOnInit(): void {
-        throw new Error('Method not implemented.');
-    }
+    ngOnInit(): void {}
+    constructor() {}
 
-    constructor(private orderService: OrderService, private supplierService: SupplierService, private productService: ProductService, private route: ActivatedRoute, private authService: AuthService, private router: Router) {
-    }
-
-    @Input() supplier; 
+    @Input() supplierInfo; 
   
 }
