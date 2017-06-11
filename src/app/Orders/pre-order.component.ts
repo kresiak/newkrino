0c50a13b43850829a214358a60ac945fd64f9250
AK <kasssalex@gmail.com>
2017-06-11 02:28:55 +0200

    Refactoring the observable chain with operators and not nested subscription + use config for max amount of 8500 limit

--- src/app/Orders/pre-order.component.ts
+++ src/app/Orders/pre-order.component.ts
@@ -4,6 +4,7 @@ import { BasketService } from './../Shared/Services/basket.service'
 import { OrderService } from './../Shared/Services/order.service'
 import { EquipeService } from '../Shared/Services/equipe.service';
 import { SupplierService } from './../Shared/Services/supplier.service'
+import { AdminService } from './../Shared/Services/admin.service'
 import { AuthenticationStatusInfo, AuthService } from './../Shared/Services/auth.service'
 import { Observable } from 'rxjs/Rx'
 
@@ -14,7 +15,7 @@ import { Observable } from 'rxjs/Rx'
 )
 export class PreOrderComponent implements OnInit {
     constructor(private orderService: OrderService, private supplierService: SupplierService, private basketService: BasketService, private route: ActivatedRoute, 
-                private authService: AuthService, private equipeService: EquipeService, private router: Router) {
+                private authService: AuthService, private equipeService: EquipeService, private adminService: AdminService, private router: Router) {
 
     }
 
@@ -29,41 +30,61 @@ export class PreOrderComponent implements OnInit {
     private isOtpOk: boolean = false
     private isSubmitButtonFree: boolean = true
 
+    private maximumSpendingAccepted: number= 500
+
     private isPageRunning: boolean= true
 
     ngOnInit(): void {
-        this.route.params.takeWhile(() => this.isPageRunning).subscribe((params: Params) => {
+        this.route.params.switchMap((params: Params) => {
             let supplierId = params['id']
-            if (supplierId) {
-                this.supplierService.getSupplier(supplierId).takeWhile(() => this.isPageRunning).subscribe(supplier => this.supplier = supplier)
-                this.productsBasketObservable = this.basketService.getAnnotatedProductsInCurrentUserBasketBySupplierWithOtp(supplierId)
-                this.productsBasketObservable.takeWhile(() => this.isPageRunning).subscribe(products => {
-                    this.productsInBasket = products
-                    this.isOtpOk = products.filter(product => product.annotation.otp && !product.annotation.otp._id).length == 0
-
-                    this.authService.getStatusObservable().takeWhile(() => this.isPageRunning).subscribe(statusInfo => {
-                        this.authorizationStatusInfo = statusInfo
-                        if (statusInfo.isGroupOrdersUser()) {
-                            this.isGroupOrdersUser = true
-                        }
-                        else {
-                            this.isGroupOrdersUser = false
-                            this.equipeService.getAnnotatedCurrentEquipe().takeWhile(() => this.isPageRunning).subscribe(eq => {
-                                if (!eq) return
-                                var total = products.map(item => item.annotation.totalPrice).reduce((a, b) => a + b, 0)
-                                this.isEnoughBudget = total < eq.annotation.amountAvailable
-                                var totalHtva = products.map(item => item.annotation.totalPriceHTva).reduce((a, b) => a + b, 0)
-                                this.isTotalUnderLimit= totalHtva < 8500
-                            })
-
-                        }
-                    });
-
-
+            return this.supplierService.getSupplier(supplierId)
+        })
+        .filter(supplier => supplier)
+        .do(supplier => {
+            this.supplier= supplier
+        })
+        .map(supplier => this.basketService.getAnnotatedProductsInCurrentUserBasketBySupplierWithOtp(supplier._id))
+        .do(productObervable => {
+            this.productsBasketObservable= productObervable
+        })
+        .switchMap(productObervable => {
+            return productObervable.takeWhile(() => this.isPageRunning)   //takeWhile necessary on each switchmap 
+        })        
+        .do(products => {
+            this.productsInBasket = products
+            this.isOtpOk = products.filter(product => product.annotation.otp && !product.annotation.otp._id).length == 0            
+        })
+        .combineLatest(
+            this.authService.getStatusObservable()
+                .do((statusInfo) => {
+                    this.authorizationStatusInfo = statusInfo
+                    this.isGroupOrdersUser= statusInfo.isGroupOrdersUser()
                 })
-
+                .map(statusInfo => statusInfo.isGroupOrdersUser()),
+            this.adminService.getLabo().map(labo => labo.data.maxOrderAmount),
+            (products, isGroupOrderUser, maxOrderAmount) => {
+                var totalHtva = products.map(item => item.annotation.totalPriceHTva).reduce((a, b) => a + b, 0)
+                return { 
+                    total : products.map(item => item.annotation.totalPrice).reduce((a, b) => a + b, 0),
+                    isTotalUnderLimit: totalHtva < maxOrderAmount,
+                    isGroupOrderUser: isGroupOrderUser
+                }
             }
-        });
+        )
+        .do(infoObject => {
+            this.isTotalUnderLimit= infoObject.isTotalUnderLimit
+        })
+        .filter(infoObject => !infoObject.isGroupOrderUser)
+        .switchMap(infoObject => {
+            return this.equipeService.getAnnotatedCurrentEquipe().filter(eq => eq).map(eq => infoObject.total < eq.annotation.amountAvailable).takeWhile(() => this.isPageRunning)  //takeWhile necessary on each switchmap 
+        })
+        .do(isEnoughBudget => {
+            this.isEnoughBudget= isEnoughBudget
+        })
+        .takeWhile(() => this.isPageRunning)
+        .subscribe(res => {
+        })
+
 
         this.groupsForSelectionObservable = this.equipeService.getAnnotatedEquipesGroups().map(groups => groups.map(group => {
             return {
