83e86aec395be07021661ce2959ac533c0dd7e69
AK <kasssalex@gmail.com>
2017-07-03 18:21:56 +0200

    PLATFORM: refactoring the compare: allow non matching

--- src/app/Platforms/platform-service-compare.component.ts
+++ src/app/Platforms/platform-service-compare.component.ts
@@ -28,16 +28,16 @@ export class PlatformServiceCompareComponent implements OnInit {
 
     ngOnInit(): void {
         this.platformService.getAnnotatedServices().map(services => services.filter(s => s.data._id === this.service1Id)[0]).takeWhile(() => this.isPageRunning).subscribe(service => {
-            this.service1= service
+            this.service1 = service
         })
         this.platformService.getAnnotatedServices().map(services => services.filter(s => s.data._id === this.service2Id)[0]).takeWhile(() => this.isPageRunning).subscribe(service => {
-            this.service2= service
+            this.service2 = service
         })
         this.platformService.getAnnotatedServiceStepsByService(this.service1Id).takeWhile(() => this.isPageRunning).subscribe(steps => {
-            this.service1Steps= steps
+            this.service1Steps = steps
         })
         this.platformService.getAnnotatedServiceStepsByService(this.service2Id).takeWhile(() => this.isPageRunning).subscribe(steps => {
-            this.service2Steps= steps
+            this.service2Steps = steps
         })
     }
 
@@ -48,4 +48,66 @@ export class PlatformServiceCompareComponent implements OnInit {
     getIndexes(arr) {
         return Array.from(arr.keys())
     }
+
+    mergeBoth(arr1, arr2, fnGetKey) {
+        function getElementsByKeys(arr, key): any[] {
+            if (!arr) return []
+            return arr.filter(elem => fnGetKey(elem) === key)
+        }
+
+        function getKeys(arr): any[] {
+            if (!arr) return []
+            return arr.map(elem => fnGetKey(elem))
+        }
+
+        function getDistinctKeys(arr1, arr2) {
+            var keys1 = getKeys(arr1)
+            var keys2 = getKeys(arr2)
+
+            var set: Set<string> = new Set()
+
+            keys1.forEach(k => {
+                if (!set.has(k)) set.add(k)
+            })
+
+            keys2.forEach(k => {
+                if (!set.has(k)) set.add(k)
+            })
+
+            return Array.from(set.values())
+        }
+
+        var x=  getDistinctKeys(arr1, arr2).map(key => {
+            return {
+                key: key,
+                arr1: getElementsByKeys(arr1, key),
+                arr2: getElementsByKeys(arr2, key) 
+            }
+        }).reduce((acc, elem) => {
+            for (var i: number=0; i<Math.max(elem.arr1.length, elem.arr2.length); i++) {
+                acc.push({
+                    key: elem.key,
+                    val1: i < elem.arr1.length ? elem.arr1[i] : {data: {}, annotation: {}},
+                    val2: i < elem.arr2.length ? elem.arr2[i] : {data: {}, annotation: {}}
+                })
+            }
+            return acc
+        }, [])
+
+        return x
+    }
+
+    mergeBothSteps(arr1, arr2) { 
+        return this.mergeBoth(arr1, arr2, step => step.data.machineId)
+    }
+
+    mergeBothProducts(arr1, arr2) { 
+        return this.mergeBoth(arr1, arr2, prod => prod.data.id)
+    }
+
+    mergeBothLabours(arr1, arr2) { 
+        return this.mergeBoth(arr1, arr2, labour => labour.data.id)
+    }
+
+
 }
