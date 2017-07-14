3f73ec32f553c4830ce47e4a0ee9c250b5160d5b
kresiak <kresiak1@gmail.com>
2017-07-14 20:40:46 +0200

    Type Script clean

--- src/app/Platforms/platform-offer-detail.component.ts
+++ src/app/Platforms/platform-offer-detail.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core'
 import { Observable } from 'rxjs/Rx'
 import { DataStore } from './../Shared/Services/data.service'
 import { PlatformService } from './../Shared/Services/platform.service'
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import * as comparatorsUtils from './../Shared/Utils/comparators'
 
 @Component(
@@ -12,22 +11,16 @@ import * as comparatorsUtils from './../Shared/Utils/comparators'
     }
 )
 export class PlatformOfferDetailComponent implements OnInit {
-    constructor (private formBuilder: FormBuilder, private dataStore: DataStore, private platformService: PlatformService) {
+    constructor ( private dataStore: DataStore, private platformService: PlatformService) {
     }
     
 @Input() offerItem
-private offersList: any
 private isPageRunning: boolean = true
 private clientsListObservable
 private clientId: string
 
     ngOnInit(): void {
 
-        this.platformService.getAnnotatedOffers().takeWhile(() => this.isPageRunning).subscribe(offers => {
-            if (!comparatorsUtils.softCopy(this.offersList, offers))
-                this.offersList= comparatorsUtils.clone(offers)            
-        })
-        
         this.clientsListObservable = this.dataStore.getDataObservable('platform.clients').takeWhile(() => this.isPageRunning).map(clients => clients.map(client => {
                 return {
                     id: client._id,
