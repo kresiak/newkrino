2f8e67dcfa434cff3dcc3fa643a87ffc16633b95
AK <kasssalex@gmail.com>
2017-10-16 18:41:06 +0200

    search box component in offer list of platforms

--- src/app/Platforms/platform-offer-list.component.ts
+++ src/app/Platforms/platform-offer-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core'
 import { NgbPanelChangeEvent } from '@ng-bootstrap/ng-bootstrap'
 import { Observable, BehaviorSubject } from 'rxjs/Rx'
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import * as comparatorsUtils from './../Shared/Utils/comparators'
 
 @Component(
@@ -11,25 +10,14 @@ import * as comparatorsUtils from './../Shared/Utils/comparators'
     }
 )
 export class PlatformOfferListComponent implements OnInit {
-    searchForm: FormGroup;
-    searchControl = new FormControl();
 
     constructor() {
-        this.searchForm = new FormGroup({
-            searchControl: new FormControl()
-        });
     }
 
     private state
     @Input() offersObservable: Observable<any>
 
     private isPageRunning: boolean = true
-
-    private nbHitsShown: number = 10
-    private nbHitsIncrement: number = 10
-    private nbHits: number
-    private nbHitsShownObservable: BehaviorSubject<number> = new BehaviorSubject<number>(this.nbHitsShown)
-
 
     private offersList: any
 
@@ -38,35 +26,23 @@ export class PlatformOfferListComponent implements OnInit {
         if (!this.state.openPanelId) this.state.openPanelId = '';
     }
 
+    fnFilter(o, txt) {
+        if (txt === '' || txt === '$') return true
+        if (txt.startsWith('$>') && +txt.slice(2)) {
+            let montant = +txt.slice(2);
+            return + o.annotation.total >= montant;
+        }
+        if (txt.startsWith('$<') && +txt.slice(2)) {
+            let montant = +txt.slice(2);
+            return + o.annotation.total <= montant;
+        }
+        return (o.data.description || '').toUpperCase().includes(txt) || (o.annotation.serviceTxt || '').toUpperCase().includes(txt) || (o.annotation.numero || '').toUpperCase().includes(txt)
+            || (o.annotation.client || '').toUpperCase().includes(txt) || (o.annotation.commercialStatus || '').toUpperCase().includes(txt)
+    }
+
+
     ngOnInit(): void {
         this.stateInit()
-
-        Observable.combineLatest(this.offersObservable, this.searchControl.valueChanges.debounceTime(400).distinctUntilChanged().startWith(''), (offers, searchTxt: string) => {
-            let txt: string = searchTxt.trim().toUpperCase();
-            if (txt === '' || txt === '$') return offers
-            return offers.filter(o => {
-                if (txt.startsWith('$>') && +txt.slice(2)) {
-                    let montant = +txt.slice(2);
-                    return + o.annotation.total >= montant;
-                }
-                if (txt.startsWith('$<') && +txt.slice(2)) {
-                    let montant = +txt.slice(2);
-                    return + o.annotation.total <= montant;
-                }
-                return (o.data.description || '').toUpperCase().includes(txt) || (o.annotation.serviceTxt || '').toUpperCase().includes(txt) || (o.annotation.numero || '').toUpperCase().includes(txt)
-                    || (o.annotation.client || '').toUpperCase().includes(txt) || (o.annotation.commercialStatus || '').toUpperCase().includes(txt)
-            })
-        }).do(offers => {
-            this.nbHits = offers.length
-        })
-            .switchMap(offers => {
-                return this.nbHitsShownObservable.map(nbItems => {
-                    return offers.slice(0, nbItems)
-                })
-            }).takeWhile(() => this.isPageRunning).subscribe(offers => {
-                if (!comparatorsUtils.softCopy(this.offersList, offers))
-                    this.offersList = comparatorsUtils.clone(offers)
-            })
     }
 
     private beforeAccordionChange($event: NgbPanelChangeEvent) {
@@ -78,16 +54,5 @@ export class PlatformOfferListComponent implements OnInit {
     ngOnDestroy(): void {
         this.isPageRunning = false
     }
-
-    resetSerachControl() {
-        this.searchControl.setValue('')
-    };
-
-    private moreHits() {
-        this.nbHitsShown += this.nbHitsIncrement
-        this.nbHitsShownObservable.next(this.nbHitsShown)
-    }
-
-
 }
 
