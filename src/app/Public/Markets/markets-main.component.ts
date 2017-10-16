1a9c6cc116c39a499601d4fbccfb712c07da8bf2
AK <kasssalex@gmail.com>
2017-10-16 10:41:14 +0200

    public market: option to add a document + protect fields readonly if not user who created it

--- src/app/Public/Markets/markets-main.component.ts
+++ src/app/Public/Markets/markets-main.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core'
+import { Component, Input, OnInit, Output, EventEmitter, ViewChild } from '@angular/core'
 import { Observable, Subscription } from 'rxjs/Rx'
 import { DataStore } from './../../Shared/Services/data.service'
-import { AuthAnoynmousService } from './../../Shared/Services/auth-anonymous.service'
-
+import { AuthAnoynmousService, SignedInStatusInfo } from './../../Shared/Services/auth-anonymous.service'
+import { UploadMetadata, FileHolder, ImageUploadComponent } from "angular2-image-upload";
 
 @Component(
     {
@@ -11,10 +11,12 @@ import { AuthAnoynmousService } from './../../Shared/Services/auth-anonymous.ser
     }
 )
 export class MarketsMainComponent implements OnInit {
+    authorizationStatusInfo: SignedInStatusInfo;
     productsObservable: Observable<any[]>;
     isPageRunning: boolean = true;
     private productsList: any[]
 
+    @ViewChild(ImageUploadComponent) imageUploadComponent: ImageUploadComponent;
 
     constructor(private dataStore: DataStore, private authAnoynmousService: AuthAnoynmousService) {
     }
@@ -23,10 +25,10 @@ export class MarketsMainComponent implements OnInit {
         return product.data.name.toUpperCase().includes(txt.toUpperCase())
             || product.data.package.toUpperCase().includes(txt.toUpperCase())
             || (product.data.items || []).map(i => (i.supplier || '').toUpperCase()).filter(supplier => supplier.includes(txt.toUpperCase())).length > 0
-            || (product.data.items || []).map(i => (i.catalogNr || '').toUpperCase()).filter(nr => nr.includes(txt.toUpperCase())).length > 0        
+            || (product.data.items || []).map(i => (i.catalogNr || '').toUpperCase()).filter(nr => nr.includes(txt.toUpperCase())).length > 0
     }
 
-    
+
 
     ngOnInit(): void {
         this.productsObservable = Observable.combineLatest(this.dataStore.getDataObservable('products.market'), this.authAnoynmousService.getAnnotatedUsers(), (products, users) => {
@@ -34,10 +36,10 @@ export class MarketsMainComponent implements OnInit {
                 return {
                     data: product,
                     annotation: {
-                        suppliersTxt: product.items.reduce((acc, i2) => acc + (acc ? ', ': '') + i2.supplier, ''),
-                        catNrTxt: product.items.reduce((acc, i2) =>  acc + ((acc && i2.catalogNr) ? ', ': '') + i2.catalogNr, ''),
+                        suppliersTxt: product.items.reduce((acc, i2) => acc + (acc ? ', ' : '') + i2.supplier, ''),
+                        catNrTxt: product.items.reduce((acc, i2) => acc + ((acc && i2.catalogNr) ? ', ' : '') + i2.catalogNr, ''),
                         items: product.items.map(item => {
-                            var theUser= users.filter(u => u.data._id === item.userId)[0]
+                            var theUser = users.filter(u => u.data._id === item.userId)[0]
                             return {
                                 data: item,
                                 annotation: {
@@ -48,6 +50,10 @@ export class MarketsMainComponent implements OnInit {
                     }
                 }
             })
+        })
+
+        this.authAnoynmousService.getStatusObservable().takeWhile(() => this.isPageRunning).subscribe(res => {
+            this.authorizationStatusInfo = res
         })
         //this.productsObservable = this.dataStore.getDataObservable('products.market') //.map(products => products.map())
     }
@@ -61,4 +67,57 @@ export class MarketsMainComponent implements OnInit {
         this.dataStore.updateData('products.market', product.data._id, product.data)
     }
 
+
+    supplierChanged(newValue, product, itemPos) {
+        product.data.items[itemPos].supplier = newValue
+        this.dataStore.updateData('products.market', product.data._id, product.data)
+    }
+
+    catalogNrChanged(newValue, product, itemPos) {
+        product.data.items[itemPos].catalogNr = newValue
+        this.dataStore.updateData('products.market', product.data._id, product.data)
+    }
+
+    priceChanged(newValue, product, itemPos) {
+        product.data.items[itemPos].price = newValue
+        this.dataStore.updateData('products.market', product.data._id, product.data)
+    }
+
+    justificationChanged(newValue, product, itemPos) {
+        product.data.items[itemPos].justification = newValue
+        this.dataStore.updateData('products.market', product.data._id, product.data)
+    }
+
+
+    private createOurFileObj(file) {
+        return {
+            name: file.name,
+            size: file.size,
+            lastModified: file.lastModified
+        }
+    }
+
+    private getServerFileName(res) {
+        var x = JSON.parse(res.serverResponse._body)
+        var filename = x ? x.filename : 'unknown'
+        return filename
+    }
+
+    onUploadFinished(product, itemPos, res) {
+        var filename = this.getServerFileName(res)
+
+        var documents = product.data.items[itemPos].documents
+        if (!documents) documents = []
+        documents.push({
+            file: this.createOurFileObj(res.file),
+            filename: filename
+        })
+        this.dataStore.updateData('products.market', product.data._id, product.data).subscribe(res => {
+            this.imageUploadComponent.deleteAll()
+        })
+    }
+
+    isUser(item): boolean {
+        return this.authorizationStatusInfo && this.authorizationStatusInfo.currentUserId === item.data.userId
+    }
 }
