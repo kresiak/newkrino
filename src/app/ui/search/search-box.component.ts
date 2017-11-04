cdf9aeee84208741bfb82b4d78ef555e975305d0
AK <kasssalex@gmail.com>
2017-11-04 23:49:03 +0100

    fixes for complex queries in search box: and has precedence on or  +  help display with parentheses

--- src/app/ui/search/search-box.component.ts
+++ src/app/ui/search/search-box.component.ts
@@ -49,22 +49,41 @@ export class SearchBoxComponent implements OnInit {
     @Output() listChanged = new EventEmitter(true);  // the true parameter is important to make the this.searchChanged.next() call asynchr and thus to avaoid the error: `ExpressionChangedAfterItHasBeenCheckedError` (https://blog.angularindepth.com/everything-you-need-to-know-about-the-expressionchangedafterithasbeencheckederror-error-e3fd9ce7dbb4)
     @Output() reportNeeded = new EventEmitter(true);
 
+    private explainedComplexQuery: string = ''
+
     private createFilterFn(searchString: string) {
-        if (! this.fnFilterObjects) return (object) => true
-        var txt= searchString.toUpperCase()
-        var andList= txt.split(' AND ').map(terme => terme.split(' OR ').map(e => e.trim()).filter(e => e != '')).filter(orList => orList && orList.length > 0)
+        if (!this.fnFilterObjects) return (object) => true
+        var txt = searchString.toUpperCase()
+        var orList = txt.split(' OR ').map(terme => terme.split(' AND ').map(e => e.trim()).filter(e => e != '')).filter(andList => andList && andList.length > 0)
+
+        var fn= txt => '<b>' + txt + '</b>'
+        var fn2= txt => ' <b>' + txt + '</b> '
+
+        this.explainedComplexQuery = orList.reduce((acc, andList) => {
+            var andText = andList.reduce((acc2, tokenTxt: string) => {
+                var isNot = tokenTxt.startsWith('NOT ')
+                var tokenTxtChanged= isNot ? (fn('[ ') + fn('NOT ') + tokenTxt.substr(4) + fn(' ]')) : tokenTxt
+                return acc2 + (acc2 ? fn2('AND') : '') + tokenTxtChanged
+            }, '')
+            var andTextChanged= (andList && andList.length > 1) ?  (fn('[ ') + andText + fn(' ]')) : andText
+            return acc + (acc ? fn2('OR') : '') + andTextChanged
+        }, '')
+
+        if (this.explainedComplexQuery.trim().toUpperCase() === txt.trim().toUpperCase()) this.explainedComplexQuery = ''      // hide it if it doesn't bring any extra info
+
         return object => {
-            return andList.reduce((isOkSoFar, orList: any[]) => {
-                if (!isOkSoFar) return false
-                var isThisOk: boolean= orList.reduce((isOrListOkSoFar, tokenTxt: string) => {
-                    if (isOrListOkSoFar) return true
-                    var isNot= tokenTxt.startsWith('NOT ')
-                    var txtToSearch= isNot ? tokenTxt.substring(4).trim() : tokenTxt
-                    var result= this.fnFilterObjects(object, txtToSearch)
+            if (!orList || orList.length === 0) return true
+            return orList.reduce((isOrListOkSoFar, andList: any[]) => {
+                if (isOrListOkSoFar) return true
+                var isThisOk: boolean = andList.reduce((isAndListOkSoFar, tokenTxt: string) => {
+                    if (!isAndListOkSoFar) return false
+                    var isNot = tokenTxt.startsWith('NOT ')
+                    var txtToSearch = isNot ? tokenTxt.substring(4).trim() : tokenTxt
+                    var result = this.fnFilterObjects(object, txtToSearch)
                     return isNot ? !result : result
-                }, false)
+                }, true)
                 return isThisOk
-            }, true)
+            }, false)
         }
     }
 
@@ -79,32 +98,32 @@ export class SearchBoxComponent implements OnInit {
 
         Observable.combineLatest(this.objectsObservable, this.searchControl.valueChanges.debounceTime(400).distinctUntilChanged().startWith(initialSearch).takeWhile(() => this.isPageRunning), (objects, searchTxt: string) => {
             this.configService.listSaveSearchText(this.objectTypeTranslationKey, searchTxt)
-            var filterFn= this.createFilterFn(searchTxt)
+            var filterFn = this.createFilterFn(searchTxt)
             return objects.filter(object => filterFn(object))
         }).do(objects => {
             this.nbHits = objects.length
             this.moneyTotal = this.fnCalculateTotal ? this.fnCalculateTotal(objects) : 0
             this.allObjects = objects
         }).switchMap(objects => {
-                if (!this.sortFunctionObservable) return Observable.from([objects])
-                var objectsCopy = comparatorsUtils.clone(objects)
-                return this.sortFunctionObservable.map(fn => {
-                    return fn ? objectsCopy.sort(fn) : objects
-                })
-        }).switchMap(objects => {
-                var objectsCopy = comparatorsUtils.clone(objects)
-                return this.isReverseObservable.map(isReverse => {
-                    return isReverse ? objectsCopy.reverse() : objects
-                })
-        }).switchMap(objects => {
-                return this.nbHitsShownObservable.map(nbItems => {
-                    return objects.slice(0, nbItems)
-                })
-        }).takeWhile(() => this.isPageRunning).subscribe(o => {
-                if (!comparatorsUtils.softCopy(this.objects, o))
-                    this.objects = comparatorsUtils.clone(o)
-                this.listChanged.next(this.objects)
+            if (!this.sortFunctionObservable) return Observable.from([objects])
+            var objectsCopy = comparatorsUtils.clone(objects)
+            return this.sortFunctionObservable.map(fn => {
+                return fn ? objectsCopy.sort(fn) : objects
             })
+        }).switchMap(objects => {
+            var objectsCopy = comparatorsUtils.clone(objects)
+            return this.isReverseObservable.map(isReverse => {
+                return isReverse ? objectsCopy.reverse() : objects
+            })
+        }).switchMap(objects => {
+            return this.nbHitsShownObservable.map(nbItems => {
+                return objects.slice(0, nbItems)
+            })
+        }).takeWhile(() => this.isPageRunning).subscribe(o => {
+            if (!comparatorsUtils.softCopy(this.objects, o))
+                this.objects = comparatorsUtils.clone(o)
+            this.listChanged.next(this.objects)
+        })
 
         this.configService.getTranslationWord('GENERAL.' + this.objectTypeTranslationKey).subscribe(txt => {
             this.objectTypeText = txt
